f3da4087c5b185c7eb3a7efa7cc5227b9014d28a
Rkmutech <ramkumar.iconao@gmail.com>
2025-05-17 17:19:15 +0530

    pizza list ui  compete

--- pizza-ui/src/Menu.js
+++ pizza-ui/src/Menu.js
@@ -1,32 +1,32 @@
 function Menu() {
- const pizzaList = [
-  { id: 1, list: "paneer-pizza", image: require("./image/panner-PIZZA.jpg") },
-  { id: 2, list: "chicken-pizza", image: require("./image/checkin-pizza.jpg") },
-  { id: 3, list: "pizza", image: require("./image/pizza.jpg") },
-  { id: 4, list: "veg-pizza", image: require("./image/veg-pizza.jpg") },
-  { id: 6, list: "veg-2-pizza", image: require("./image/veg-2 pizza.jpg") },
-  { id: 7, list: "veg-3-pizza", image: require("./image/veg-3 pizza.jpg") },
-];
+  const pizzaList = [
+    { id: 1, list: "paneer-pizza", image: require("./image/paneer-PIZZA.jpg") },
+    { id: 2, list: "chicken-pizza", image: require("./image/checkin-pizza.jpg") },
+    { id: 3, list: "pizza", image: require("./image/pizza.jpg") },
+    { id: 4, list: "veg-pizza", image: require("./image/veg-pizza.jpg") },
+    { id: 6, list: "veg-2-pizza", image: require("./image/veg-2-pizza.jpg") },
+    { id: 7, list: "veg-3-pizza", image: require("./image/veg-3-pizza.jpg") },
+  ];
 
   return (
-    <>
-      <h2>Pizza Menu</h2>
-      <ul>
+    <div>
+      <h6 className="pizzaheader">Pizza Menu</h6>
+      <div className="pizzaCard">
         {pizzaList.map((pizza) => (
-          <li key={pizza.id}>
+          <div className="pizzaList" key={pizza.id}>
             {pizza.image && (
               <img
                 src={pizza.image}
                 alt={pizza.list}
-                width="100"
-                style={{ marginRight: "10px" }}
+                style={{ width: "35%", borderRadius: "8px" }}
               />
             )}
-            {pizza.list}
-          </li>
+            <h4>{pizza.list}</h4>
+          </div>
         ))}
-      </ul>
-    </>
+      </div>
+    </div>
   );
 }
+
 export default Menu;
